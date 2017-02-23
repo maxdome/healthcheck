c36bbb7803a278ab24bd03fd3d02b285b9a0d63b
Fabian Schneider <fabian.schneider@maxdome.de>
2017-02-23 16:43:37 +0100

    Update BBE and IM checkhelpers to use `url`

--- src/checkhelper/bbe.js
+++ src/checkhelper/bbe.js
@@ -3,8 +3,10 @@
 const request = require('superagent');
 
 module.exports = options => callback => {
+  const baseURL = options.url || (options.protocol + '://' + options.hostname + '/' + options.path);
+
   request
-    .get(options.protocol + '://' + options.hostname + '/' + options.path + '/admin/monitoring/lbcheck')
+    .get(baseURL + '/admin/monitoring/lbcheck')
     .timeout(5000)
     .end(callback);
 };
