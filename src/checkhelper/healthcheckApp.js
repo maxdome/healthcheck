5518910cce38a20fe4edc77601843147b8d8af69
Christian Neuhäuser <chris@type10.com>
2017-12-11 10:36:41 +0100

    Makes the path customizable

--- src/checkhelper/healthcheckApp.js
+++ src/checkhelper/healthcheckApp.js
@@ -6,7 +6,7 @@ module.exports = options => callback => {
   options.protocol = options.protocol ? options.protocol.split(':')[0] : '';
 
   request
-    .get(options.protocol + '://' + options.hostname + '/health/app')
+    .get(options.protocol + '://' + options.hostname + (options.path || '/health/app'))
     .timeout(5000)
     .end(callback);
 };
